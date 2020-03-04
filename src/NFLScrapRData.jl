5c93ba8aceb212ecb3ca649a5ed4ec4ed0320b10
Kyle S. Johnston <KyleSJohnston@users.noreply.github.com>
2020-03-03 21:39:03 -0500

    Use artifacts to get NFLScrapR data

--- src/NFLScrapRData.jl
+++ src/NFLScrapRData.jl
@@ -7,9 +7,79 @@ module NFLScrapRData
 
 using  CSV
 using  DataFrames
-using  HTTP
+using  Pkg.Artifacts
 
-export game, playbyplay
+export game, playbyplay, pre, reg, post, seasons, seasonparts, validseason, validpart
+
+"""
+seasons with nflscrapR data
+"""
+const SEASONS = tuple(2009:2019...)
+
+"""
+parts of a single season
+"""
+@enum SeasonPart pre reg post
+
+"""
+returns true if `season` is valid
+"""
+function validseason(season::Int)
+    return season in SEASONS
+end
+
+"""
+return an array of season parts for a valid season
+"""
+function seasonparts(season::Int)
+    validseason(season) || error("Invalid season: $season")
+    # if season == 2020:
+    #     return (pre,)
+    # end
+    return instances(SeasonPart)
+end
+
+"""
+returns true if `part` is valid for `season`
+"""
+function validpart(season::Int, part::SeasonPart)
+    return part in seasonparts(season)
+end
+
+"""
+    playbyplay(season::Int, part::SeasonPart)
+
+Create a dataframe of play-by-play data for `part` of `season`.
+Supplying a value for `root` will override the default, allowing users to
+reference a local clone of the nflscrapR-data repository.
+"""
+function playbyplay(season::Int, part::SeasonPart)
+    validpart(season, part) || error("Invalid part ($part) for season $season")
+    path = season_artifact(season)
+    return CSV.File(joinpath(path, "pbp_$part.csv"), missingstring="NA") |> DataFrame!
+end
+
+"""
+    game(season::Int, part::SeasonPart)
+
+Create a dataframe of game data for `part` of `season`.
+Supplying a value for `root` will override the default, allowing users to
+reference a local clone of the nflscrapR-data repository.
+"""
+function game(season::Int, part::SeasonPart)
+    validpart(season, part) || error("Invalid part ($part) for season $season")
+    path = season_artifact(season)
+    return CSV.File(joinpath(path, "game_$part.csv"), missingstring="NA") |> DataFrame!
+end
+
+const artifact_toml = joinpath(@__DIR__, "..", "Artifacts.toml")
+
+"""
+Aliases for the parts of the season
+"""
+const partaliases = Dict{SeasonPart,AbstractString}(
+    pre => "pre", reg => "regular", post => "post"
+)
 
 """
 The root of the nflscrapR-data GitHub repo, after the raw redirect.
@@ -17,67 +87,53 @@ The root of the nflscrapR-data GitHub repo, after the raw redirect.
 const reporoot = "https://raw.githubusercontent.com/ryurko/nflscrapR-data/master/"
 
 """
-Aliases for the parts of the season
-"""
-const partaliases = Dict(
-    "pre" => "pre", "reg" => "regular", "post" => "post"
-)
-
-"""
-    playbyplaypath(season::Int, part::AbstractString; root::AbstractString=reporoot)
+    playbyplaypath(season::Int, part::SeasonPart; root::AbstractString=reporoot)
 
 Create a string representing the path play-by-play data for `part` of `season`.
 Supplying a value for `root` will override the default, allowing users to
 reference a local clone of the nflscrapR-data repository.
 """
-function playbyplaypath(season::Int, part::AbstractString; root::AbstractString=reporoot)
+function playbyplaypath(season::Int, part::SeasonPart; root::AbstractString=reporoot)
     haskey(partaliases, part) || error("invalid part: $(part)")
     return joinpath(root, "play_by_play_data", "$(partaliases[part])_season", "$(part)_pbp_$(season).csv")
 end
 
 """
-    gamepath(season::Int, part::AbstractString; root::AbstractString=reporoot)
+    gamepath(season::Int, part::SeasonPart; root::AbstractString=reporoot)
 
 Create a string representing the path to game data for `part` of `season`.
 Supplying a value for `root` will override the default, allowing users to
 reference a local clone of the nflscrapR-data repository.
 """
-function gamepath(season::Int, part::AbstractString; root::AbstractString=reporoot)
+function gamepath(season::Int, part::SeasonPart; root::AbstractString=reporoot)
     haskey(partaliases, part) || error("invalid part: $(part)")
     return joinpath(root, "games_data", "$(partaliases[part])_season", "$(part)_games_$(season).csv")
 end
 
-function load(path::AbstractString)
-    if occursin(r"^http(s)?://", path)
-        source = HTTP.get(path).body
-        return CSV.File(source; missingstring="NA") |> DataFrame!
-    else
-        return CSV.File(path; missingstring="NA") |> DataFrame!
+"""
+Adapted from https://julialang.github.io/Pkg.jl/v1/artifacts/#Using-Artifacts-1
+
+Artifacts are entire directories of data, so we should have one NFLScrapR
+directory for each year (tradeoff between amount of data and likelihood of change).
+"""
+function season_artifact(season::Int; redownload::Bool=false, root::AbstractString=reporoot)
+    validseason(season) || error("Invalid season: $season")
+    an = "nflscrapR_$(season)"
+    ah = artifact_hash(an, artifact_toml)
+    # If the name was not bound, or the hash it was bound to does not exist, create it!
+    if redownload || ah == nothing || !artifact_exists(ah)
+        # create_artifact() returns the content-hash of the artifact directory once we're finished creating it
+        ah = create_artifact() do artifact_dir
+            # We create the artifact by simply downloading a few files into the new artifact directory
+            for part in seasonparts(season)
+                download(gamepath(season, part, root=root), joinpath(artifact_dir, "game_$(part).csv"))
+                download(playbyplaypath(season, part, root=root), joinpath(artifact_dir, "pbp_$(part).csv"))
+            end
+        end
+
+        bind_artifact!(artifact_toml, an, ah, force=true)
     end
+    return artifact_path(ah)
 end
-
-
-"""
-    playbyplay(season::Int, part::AbstractString; root::AbstractString=reporoot)
-
-Create a dataframe of play-by-play data for `part` of `season`.
-Supplying a value for `root` will override the default, allowing users to
-reference a local clone of the nflscrapR-data repository.
-"""
-function playbyplay(season::Int, part::AbstractString; root::AbstractString=reporoot)
-    return load(playbyplaypath(season, part, root=root))
-end
-
-"""
-    game(season::Int, part::AbstractString; root::AbstractString=reporoot)
-
-Create a dataframe of game data for `part` of `season`.
-Supplying a value for `root` will override the default, allowing users to
-reference a local clone of the nflscrapR-data repository.
-"""
-function game(season::Int, part::AbstractString; root::AbstractString=reporoot)
-    return load(gamepath(season, part, root=root))
-end
-
 
 end  # module NFLScrapRData
--- test/NFLScrapRData.jl
+++ test/NFLScrapRData.jl
@@ -4,29 +4,31 @@ using  DataFrames
 using  Test
 
 using  NFLTables.NFLScrapRData
-import NFLTables.NFLScrapRData: gamepath, playbyplaypath
-
-seasons = 2009:2018  # 2019 is still in progress, omit from testing
-parts = ("pre", "reg", "post")
+import NFLTables.NFLScrapRData: gamepath, playbyplaypath, SEASONS, seasonparts, season_artifact
 
 @testset "nflscrapR-data path tests" begin
-    for season in seasons, part in parts
+    for season in SEASONS, part in seasonparts(season)
         path = playbyplaypath(season, part)
-        @test typeof(path) <: String
+        @test isa(path, String)
         path = gamepath(season, part)
-        @test typeof(path) <: String
+        @test isa(path, String)
     end
 end
 
-if haskey(ENV, "NFLSCRAPR_DATA")
-    # only run full data tests if the data is local
-    @testset "nflscrapR-data df tests" begin
-        for season in seasons, part in parts
-            df = playbyplay(season, part, root=ENV["NFLSCRAPR_DATA"])
-            @test typeof(df) == DataFrame
-            df = game(season, part, root=ENV["NFLSCRAPR_DATA"])
-            @test typeof(df) == DataFrame
-        end
+@testset "nflscrapR-data artifacts" begin
+    for season in SEASONS
+        path = season_artifact(season, redownload=false)
+        @test isa(path, String)
+    end
+    @test_throws ErrorException season_artifact(3001)
+end
+
+@testset "nflscrapR-data df tests" begin
+    for season in SEASONS, part in seasonparts(season)
+        df = playbyplay(season, part)
+        @test typeof(df) == DataFrame
+        df = game(season, part)
+        @test typeof(df) == DataFrame
     end
 end
 
