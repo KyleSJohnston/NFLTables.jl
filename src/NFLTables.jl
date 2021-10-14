65c4b0d73e947f0316b3773bef93c7ca89f1f9e0
Kyle S. Johnston <KyleSJohnston@users.noreply.github.com>
2021-10-14 19:55:53 -0400

    No need for artifacts module

--- src/NFLTables.jl
+++ src/NFLTables.jl
@@ -7,12 +7,11 @@ ARTIFACT_TOML = joinpath(splitdir(@__DIR__)[1], "Artifacts.toml")
 
 # include submodules
 include("enumerations.jl")
-include("artifacts.jl")  # required for schedule and nflscrapR data
 include("schedules.jl")
 include("NFLScrapRData.jl")
 include("NFLFastR.jl")
 
 # export the API
-export nflscrapRplaybyplay, nflscrapRgame, POST, PRE, REG, nflschedule, SeasonPart, SuperBowl
+export POST, PRE, REG, SeasonPart, SuperBowl
 
 end # module
